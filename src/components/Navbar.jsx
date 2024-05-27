d30f76df2ada5537f5a325439ab0db8aaf7dabd2
0MadGaming0 <madhavsp05@gmail.com>
2024-05-27 18:08:02 +0530

    Metro app

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,11 @@ const Navbar = () => {
         <div>
             <nav class="navbar navbar-expand-lg bg-info">
                 <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <img src="https://static.vecteezy.com/system/resources/previews/018/859/280/non_2x/train-rail-logo-symbol-flat-color-illustration-locomotive-vector.jpg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/>
+                            Bootstrap
+                    </a>
+
                     <a class="navbar-brand" href="#">Metro App</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
